11ec4845dab46c0ec8fa16cc85e14d2cd8e55187
Har Dhillon <har7173@gmail.com>
2020-11-11 14:35:09 +0000

    added controller index and show

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,25 @@
+class RestaurantsController < ApplicationController
+  before_action :find_restaurant, only: [ :show ]
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+
+
+
+
+  
+  private 
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating, :phone_number)
+  end
+
+end
