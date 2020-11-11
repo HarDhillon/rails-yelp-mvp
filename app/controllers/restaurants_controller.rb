9a0dd12a1e909c05990aa1688af53f5ca59ae667
Har Dhillon <har7173@gmail.com>
2020-11-11 16:17:51 +0000

    working restaurant and reviews system

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,10 +7,18 @@ class RestaurantsController < ApplicationController
   def show
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
 
-
-
-
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
   
   private 
 
@@ -19,7 +27,7 @@ class RestaurantsController < ApplicationController
   end
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :rating, :phone_number)
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
   end
 
 end
